d91506962feed42ec04deb633cb31a5e5df22e30
sandorbence <szaniszlo47@gmail.com>
2023-05-14 11:27:18 +0200

    added keyboard events, has to be imroved tho

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -3,14 +3,24 @@ class Player {
         this.position = {
             x: 100,
             y: 100
-        }
+        };
         this.width = 100;
         this.height = 100;
         this.color = "red";
+        this.velocity = {
+            x: 0,
+            y: 0
+        };
+        this.acceleration = {
+            x:0,
+            y:0
+        };
+        this.gravity = 1;
     }
 
     Update() {
-        this.position.y++;
+        this.position.y += this.velocity.y;
+        this.velocity.y++;
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -8,10 +8,28 @@ const player = new Player();
 
 function animate() {
   window.requestAnimationFrame(animate);
-  player.Update();
-  c2d.clearRect(0, 0, canvas.width, canvas.height);
+  if (player.position.y + player.height < canvas.height) {
+    player.Update();
+  };
+  c2d.fillStyle = "white";
+  c2d.fillRect(0, 0, canvas.width, canvas.height);
   c2d.fillStyle = player.color;
   c2d.fillRect(player.position.x, player.position.y, player.width, player.height);
 }
 
 animate();
+
+window.addEventListener("keydown", move);
+
+function move(event) {
+  if (event.keyCode == 38) {
+    player.velocity.y -= 30;
+    player.position.y -= 250;
+  }
+  if (event.keyCode == 37) {
+    player.position.x -= 3;
+  }
+  if (event.keyCode == 39) {
+    player.position.x += 3;
+  }
+}
