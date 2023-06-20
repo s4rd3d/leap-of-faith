7a23d08f2355377715264e09e3a5f294072cdbfb
Sándor Dávid <sandordavid1124@gmail.com>
2023-06-20 23:16:07 +0200

    refactor: Introduce top level controller
    This commit does a major refactoring in the source code. Amongst
    others, the following changes were made:
    
    - Added top level controller that controls possible game states.
    - Refactored the directory structure.
    - Split main menu into a base class and a specific class.
    - Added a new Game class to group together game related logic.

---
+++ src/game/game.js
@@ -0,0 +1,54 @@
+import World from './world';
+import Render from './render';
+
+class Game {
+  constructor(controllerCallback, canvas) {
+    // Game controller callback function
+    this.controllerCallback = controllerCallback;
+
+    // User event handling
+    this.inputController = {
+      'ArrowUp': {
+        pressed: false,
+        handle: () => this.world.getPlayer().jump(),
+      },
+      'ArrowLeft': {
+        pressed: false,
+        handle: () => this.world.getPlayer().moveLeft(),
+      },
+      'ArrowRight': {
+        pressed: false,
+        handle: () => this.world.getPlayer().moveRight(),
+      },
+    };
+
+    this.world = new World();
+    this.render = new Render(canvas, this.world, this.inputController);
+  }
+
+  createGame() {
+    this.world.generate();
+    this.render.animate();
+  }
+
+  startGame() {
+    this.addEventListeners();
+  }
+
+  // Register user input listeners
+  addEventListeners() {
+    window.addEventListener('keydown', (event) => {
+      if (this.inputController[event.key]) {
+        this.inputController[event.key].pressed = true;
+      }
+    });
+
+    window.addEventListener('keyup', (event) => {
+      if (this.inputController[event.key]) {
+        this.inputController[event.key].pressed = false;
+      }
+    });
+  }
+}
+
+export default Game;
---
+++ src/game/render.js
@@ -0,0 +1,75 @@
+import collide from './utils';
+
+class Render {
+  constructor(canvas, world, controller) {
+    this.canvas = canvas;
+    this.world = world;
+    this.context = canvas.getContext('2d');
+    this.controller = controller;
+  }
+
+  animate() {
+    window.requestAnimationFrame(() => { this.doAnimate(); });
+  }
+
+  doAnimate() {
+    Object.keys(this.controller).forEach((key) => {
+      if (this.controller[key].pressed) {
+        this.controller[key].handle();
+      }
+    });
+
+    // Get the position of the player
+    const player = this.world.getPlayer();
+    player.step();
+
+    // Get the position of all current elements in world
+    this.world.getObjects().forEach((element) => {
+      element.step();
+    });
+
+    // Set the `onGround` property to false initially. If the player collides
+    // with an object, then set it to true.
+    player.onGround = false;
+
+    // Collision detection and handling for the player
+    this.world.getObjects().forEach((element) => {
+      // Detect collision
+      if (collide(player, element)) {
+        // Handle collision
+        player.handleCollision(element);
+        element.handleCollision(player);
+      }
+    });
+
+    // If the player is higher than the middle of the canvas, push objects down.
+    if (player.position.y < this.canvas.height / 2 && player.velocity.y < 0) {
+      // Push down world objects.
+      this.world.moveObjects();
+
+      // Push down the player.
+      player.position.y -= player.velocity.y;
+
+      // Add new platforms to the top of the canvas.
+      this.world.generatePlatforms();
+
+      // Remove platforms from the world if they are out of the viewport.
+      this.world.filterObjects();
+    }
+
+    // Clear all previously drawn elements
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    // Draw the player
+    player.render(this.context);
+
+    // Draw all elements in world
+    this.world.getObjects().forEach((element) => {
+      element.render(this.context);
+    });
+
+    window.requestAnimationFrame(() => { this.doAnimate(); });
+  }
+}
+
+export default Render;
